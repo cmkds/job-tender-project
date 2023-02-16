707aa97cc648f69b027546c2f8c749547a8cb01e
cmkds <107685502+cmkds@users.noreply.github.com>
2023-02-16 14:53:25 +0900

    refec 네이버 로그인 로딩페이지 이동시 회원가입 페이지 안보이도록 설정

--- FE/src/pages/NaverLogin.js
+++ FE/src/pages/NaverLogin.js
@@ -27,6 +27,8 @@ const NaverLogin = () => {
   const navigate = useNavigate();
   const CODE = param.get("code");
   const [userId, setUserId] = useState();
+
+  const [check, setCheck] = useState(false);
   useEffect(() => {
     axios
       .get(`/api/account/naver?code=${CODE}&state=1`)
@@ -74,6 +76,7 @@ const NaverLogin = () => {
       ...state,
       memberProfile: `https://xsgames.co/randomusers/assets/avatars/pixel/${imageNum}.jpg`,
     });
+    setCheck(true);
   }, []);
 
   // 받은 파라미터 쿼리 값으로 보내서
@@ -100,90 +103,96 @@ const NaverLogin = () => {
   };
   return (
     <div>
-      <Card
-        sx={{
-          maxWidth: "70%",
-          margin: "auto",
-          marginTop: "20%",
-        }}
-      >
-        <div style={{ paddingTop: "5%", paddingLeft: "5%", fontSize: 24 }}>
-          닉네임
-        </div>
-        <div style={{ display: "flex" }}>
-          <TextField
-            ref={nicknameInput}
-            name="nickname"
-            value={state.nickname}
-            color="error"
-            onChange={handleChangeState}
-            placeholder="최대 10 글자"
-            inputProps={{
-              maxLength: 10,
-              style: { fontFamily: "GangwonEduAll" },
+      {check ? (
+        <div>
+          <Card
+            sx={{
+              maxWidth: "70%",
+              margin: "auto",
+              marginTop: "20%",
             }}
-            style={{ margin: "auto", display: "flex", width: "90%" }}
-          />
+          >
+            <div style={{ paddingTop: "5%", paddingLeft: "5%", fontSize: 24 }}>
+              닉네임
+            </div>
+            <div style={{ display: "flex" }}>
+              <TextField
+                ref={nicknameInput}
+                name="nickname"
+                value={state.nickname}
+                color="error"
+                onChange={handleChangeState}
+                placeholder="최대 10 글자"
+                inputProps={{
+                  maxLength: 10,
+                  style: { fontFamily: "GangwonEduAll" },
+                }}
+                style={{ margin: "auto", display: "flex", width: "90%" }}
+              />
+            </div>
+            <div style={{ paddingTop: "5%", paddingLeft: "5%", fontSize: 24 }}>
+              상태 메세지
+            </div>
+            <div style={{ display: "flex" }}>
+              <TextField
+                ref={memberStateInput}
+                name="memberState"
+                value={state.memberState}
+                onChange={handleChangeState}
+                multiline
+                rows={4}
+                placeholder="최대 20 글자"
+                color="error"
+                inputProps={{
+                  maxLength: 20,
+                  style: { fontFamily: "GangwonEduAll" },
+                }}
+                style={{ margin: "auto", width: "90%", display: "flex" }}
+              />
+            </div>
+            <Button
+              variant="contained"
+              style={{
+                display: "flex",
+                margin: "auto",
+                width: "90%",
+                height: "40px",
+                backgroundColor: "#FFB9B9",
+                marginBottom: "10%",
+                marginTop: "10%",
+              }}
+              onClick={signUp}
+            >
+              <p style={{ fontSize: "150%", fontFamily: "GangwonEduAll" }}>
+                가입하기
+              </p>
+            </Button>
+            <Button
+              variant="contained"
+              style={{
+                display: "flex",
+                margin: "auto",
+                width: "90%",
+                height: "75px",
+                backgroundColor: "#FFB9B9",
+                marginBottom: "10%",
+                marginTop: "10%",
+                wordBreak: "keep-all",
+              }}
+              // here
+              //
+              onClick={() => navigate(`/download/1`)}
+            >
+              <p style={{ fontSize: "150%", fontFamily: "GangwonEduAll" }}>
+                가입하고 보관함에 사진 저장하러 가기
+              </p>
+            </Button>
+          </Card>
+          <BottomBar />
         </div>
-        <div style={{ paddingTop: "5%", paddingLeft: "5%", fontSize: 24 }}>
-          상태 메세지
-        </div>
-        <div style={{ display: "flex" }}>
-          <TextField
-            ref={memberStateInput}
-            name="memberState"
-            value={state.memberState}
-            onChange={handleChangeState}
-            multiline
-            rows={4}
-            placeholder="최대 20 글자"
-            color="error"
-            inputProps={{
-              maxLength: 20,
-              style: { fontFamily: "GangwonEduAll" },
-            }}
-            style={{ margin: "auto", width: "90%", display: "flex" }}
-          />
-        </div>
-        <Button
-          variant="contained"
-          style={{
-            display: "flex",
-            margin: "auto",
-            width: "90%",
-            height: "40px",
-            backgroundColor: "#FFB9B9",
-            marginBottom: "10%",
-            marginTop: "10%",
-          }}
-          onClick={signUp}
-        >
-          <p style={{ fontSize: "150%", fontFamily: "GangwonEduAll" }}>
-            가입하기
-          </p>
-        </Button>
-        <Button
-          variant="contained"
-          style={{
-            display: "flex",
-            margin: "auto",
-            width: "90%",
-            height: "75px",
-            backgroundColor: "#FFB9B9",
-            marginBottom: "10%",
-            marginTop: "10%",
-            wordBreak: "keep-all",
-          }}
-          // here
-          //
-          onClick={() => navigate(`/download/1`)}
-        >
-          <p style={{ fontSize: "150%", fontFamily: "GangwonEduAll" }}>
-            가입하고 보관함에 사진 저장하러 가기
-          </p>
-        </Button>
-      </Card>
-      <BottomBar />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
